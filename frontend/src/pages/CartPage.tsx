bc13239ef3adbb66a05380aed55a66ea80e170fb
falinantenaina1 <nantenainaranaivojaona96@gmail.com>
2025-09-27 20:56:25 +0300

    Update CartStore

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -1,32 +1,89 @@
 import { Section } from "@/components/Section";
-import { useCartStore } from "@/stores/useCartStore";
+import { useCartStore, type productWithQuanity } from "@/stores/useCartStore";
+import { Minus, Plus, X } from "lucide-react";
 
 const CartPage = () => {
-  const { carts } = useCartStore();
-  /*  const [tab, setTabs] = useState(""); */
-  console.log(carts);
+  const { carts, removeItem, increase, decrease } = useCartStore();
+
+  const handleDecrease = (product: productWithQuanity) => {
+    if (product.quantity === 1) {
+      return removeItem(product);
+    }
+    decrease(product._id);
+  };
 
   return (
     <>
       <Section className="section">
         <h2>cart</h2>
         <div>
-          <table>
-            <thead>
-              <tr>
-                <td>Product</td>
+          <table className="table-auto">
+            <thead className="border-b border-black font-semibold">
+              <tr className="text-center">
+                <td className="text-left">Product</td>
                 <td>Quantity</td>
                 <td>Price</td>
                 <td>Subtotal</td>
               </tr>
             </thead>
             <tbody>
-              {carts.map((cart) => (
-                <tr key={cart._id}>
-                  <td>{cart.name}</td>
-                  <td>{cart.quantity}</td>
-                  <td>${cart.price}</td>
-                  <td>${cart.price * cart.quantity}</td>
+              {carts.map((product) => (
+                /* Product */
+                <tr key={product._id}>
+                  <td className="py-2 pr-4">
+                    <div className="flex items-center gap-x-4">
+                      <div className="flex size-20 items-center justify-center p-4">
+                        <img
+                          className=""
+                          src={product.imageUrl}
+                          alt={`${product.name} picture`}
+                        />
+                      </div>
+                      <div className="col-span-2 flex flex-col justify-between space-y-3">
+                        <div className="space-y-2">
+                          <h3 className="text-sm font-medium">
+                            {product.name}
+                          </h3>
+                          {product.color && (
+                            <p className="text-sm">
+                              color: <span>{product.color}</span>
+                            </p>
+                          )}
+                        </div>
+                        <button
+                          className="flex cursor-pointer text-xs hover:text-red-600"
+                          onClick={() => removeItem(product)}
+                        >
+                          <X className="size-4" />
+                          <span>Remove</span>
+                        </button>
+                      </div>
+                    </div>
+                  </td>
+                  {/* Quantity */}
+                  <td>
+                    <div className="flex w-max items-center justify-between gap-x-2 rounded-xs border border-black/50 px-2 py-0.5">
+                      <button className="cursor-pointer">
+                        <Minus
+                          strokeWidth={1}
+                          className="size-5 text-black"
+                          onClick={() => handleDecrease(product)}
+                        />
+                      </button>
+                      {product.quantity}
+                      <button className="cursor-pointer">
+                        <Plus
+                          strokeWidth={1}
+                          className="size-5 text-black"
+                          onClick={() => increase(product._id)}
+                        />
+                      </button>
+                    </div>
+                  </td>
+                  <td>
+                    <div className="px-4">${product.price}</div>
+                  </td>
+                  <td>${(product.price * product.quantity).toFixed(2)}</td>
                 </tr>
               ))}
             </tbody>
--- frontend/src/stores/useCartStore.ts
+++ frontend/src/stores/useCartStore.ts
@@ -1,8 +1,9 @@
 import type { Product } from "@/types";
 import toast from "react-hot-toast";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-type productWithQuanity = Product & { quantity: number };
+export type productWithQuanity = Product & { quantity: number };
 
 type CartStoreType = {
   carts: productWithQuanity[];
@@ -10,50 +11,72 @@ type CartStoreType = {
   removeItem: (product: Product) => void;
   increase: (id: Product["_id"]) => void;
   decrease: (product: Product["_id"]) => void;
+  clearCart: () => void;
+  total: () => number;
+  countItems: () => number;
 };
 
-export const useCartStore = create<CartStoreType>((set) => ({
-  carts: [],
+export const useCartStore = create<CartStoreType>()(
+  persist(
+    (set, get) => ({
+      carts: [],
+      /* Add Cart */
+      addItem: (product) => {
+        set((state) => {
+          const existing = state.carts.find((item) => item._id === product._id);
 
-  addItem: (product) => {
-    set((state) => {
-      const existing = state.carts.find((item) => item._id === product._id);
+          if (existing) {
+            return {
+              carts: state.carts.map((item) =>
+                item._id === product._id
+                  ? { ...item, quantity: item.quantity + 1 }
+                  : item,
+              ),
+            };
+          }
 
-      if (existing) {
-        return {
+          return { carts: [...state.carts, { ...product, quantity: 1 }] };
+        });
+
+        toast.success("Product added to cart");
+      },
+      /* Remove Cart */
+      removeItem: (product) => {
+        console.log("remove item");
+        set((state) => ({
+          carts: state.carts.filter((item) => item._id !== product._id),
+        }));
+      },
+      /* Increate item count */
+      increase: (id) => {
+        set((state) => ({
           carts: state.carts.map((item) =>
-            item._id === product._id
-              ? { ...item, quantity: item.quantity + 1 }
-              : item,
+            item._id === id ? { ...item, quantity: item.quantity + 1 } : item,
           ),
-        };
-      }
+        }));
+      },
+      /*Decrease item count */
+      decrease: (id) => {
+        set((state) => ({
+          carts: state.carts.map((item) => {
+            if (item._id === id) {
+              const newQuantity = item.quantity - 1;
 
-      return { carts: [...state.carts, { ...product, quantity: 1 }] };
-    });
+              return { ...item, quantity: newQuantity >= 0 ? newQuantity : 0 };
+            }
+            return item;
+          }),
+        }));
+      },
 
-    toast.success("Product added to cart");
-  },
-
-  removeItem: (product) => {
-    set((state) => ({
-      carts: state.carts.filter((item) => item._id !== product._id),
-    }));
-  },
-
-  increase: (id) => {
-    set((state) => ({
-      carts: state.carts.map((item) =>
-        item._id === id ? { ...item, quantity: item.quantity + 1 } : item,
-      ),
-    }));
-  },
-
-  decrease: (id) => {
-    set((state) => ({
-      carts: state.carts.map((item) =>
-        item._id === id ? { ...item, quantity: item.quantity - 1 } : item,
-      ),
-    }));
-  },
-}));
+      clearCart: () => set({ carts: [] }),
+      total: () =>
+        get().carts.reduce((s, item) => s + item.price * item.quantity, 0),
+      countItems: () => get().carts.length,
+    }),
+    {
+      name: "elegant-cart",
+      /* storage: createJSONStorage(() => sessionStorage), */
+    },
+  ),
+);
