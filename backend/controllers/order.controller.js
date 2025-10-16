f4ff9521c20f440aadac89d0108927afaf1496ec
falinantenaina1 <nantenainaranaivojaona96@gmail.com>
2025-10-16 08:19:19 +0300

    Update createOrder controller

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,9 +1,38 @@
+import Order from "../models/order.model.js";
+import Product from "../models/product.model.js";
 export const createOrder = async (req, res) => {
   try {
     const userId = req.user._id;
-    const { carts, total, shippingType } = req.body;
+    const { carts, total, shippingType, shippingdAddress } = req.body;
 
-    res.status(201).json(carts);
+    const items = [];
+
+    for (const item of carts) {
+      const product = await Product.findById(item._id);
+      if (!product) {
+        return res.status(400).json({ error: "Product not found" });
+      }
+
+      const item = {
+        product: product._id,
+        quantity: item.quantity,
+        priceAtPurchase: product.price,
+      };
+
+      items.push(item);
+    }
+
+    const order = new Order({
+      user: userId,
+      items,
+      totaltotalAmount: total,
+      shippingdAddress,
+      shippingType,
+    });
+
+    await order.save();
+
+    res.status(201).json(order);
   } catch (error) {
     console.log("Error in createOrder controller:", error);
     res.status(500).json({ error: "Internal server error" });
