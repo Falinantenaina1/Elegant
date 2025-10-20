6fc6f0c9ca596a30070478205bef4c06c5023fad
falinantenaina1 <nantenainaranaivojaona96@gmail.com>
2025-10-20 21:40:28 +0300

    Change user page to be the layaout of user route

--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -1,73 +1,65 @@
-import Loading from "@/components/Loading";
 import { Section } from "@/components/Section";
+import { useOrderStore } from "@/stores/useOrderStore";
 import { useUserStore } from "@/stores/useUserStore";
-import React, { Suspense, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-const AccountDetails = React.lazy(
-  () => import("@/components/users/AccountDetails"),
-);
-
-const Address = React.lazy(() => import("@/components/users/Address"));
-
-const tabsMenu = ["Account", "Address", "Orders"];
+import { useEffect } from "react";
+import { Link, NavLink, Outlet } from "react-router-dom";
+import NotFound from "./NotFound";
 
 const UserPage = () => {
   const { user, logout } = useUserStore();
 
-  const navigate = useNavigate();
-
-  const [tabs, setTabs] = useState("Account");
+  const { getUserOrder } = useOrderStore();
 
   useEffect(() => {
-    if (!user) {
-      navigate("/");
-    }
-  }, [user]);
+    getUserOrder();
+  }, [getUserOrder]);
+
+  if (!user) return <NotFound />;
 
   return (
     <Section className="section">
       <h2 className="h2">My Account</h2>
-      <div className="flex gap-x-4">
+      <div className="flex gap-x-4 max-md:flex-col">
         {/* User Card */}
-        <div className="w-[16rem] bg-[#F3F5F7] p-4">
+        <div className="w-full bg-[#F3F5F7] p-4 md:w-[16rem]">
           <div className="flex flex-col items-center space-y-2">
             <div className="size-20 rounded-full bg-gray-900"></div>
             <h3>{user?.lastname}</h3>
           </div>
           {/* User Tabs */}
-          <div>
-            <ul>
-              {tabsMenu.map((tab) => (
-                <li
-                  key={tab}
-                  className={`${tabs === tab ? "border-black font-semibold" : "border-transparent hover:font-medium"} cursor-pointer border-b-2 py-2`}
-                  onClick={() => setTabs(tab)}
-                >
-                  {tab}
-                </li>
-              ))}
-              <li
-                className="cursor-pointer hover:font-semibold"
-                onClick={logout}
-              >
-                Logout
-              </li>
-            </ul>
+          <div className="flex flex-col space-y-4">
+            <NavLink
+              to={"/user"}
+              className={({ isActive }) =>
+                isActive ? "font-bold" : "hover:text-bold font-normal"
+              }
+            >
+              Account
+            </NavLink>
+            <NavLink
+              to={"/user/address"}
+              className={({ isActive }) =>
+                isActive ? "font-bold" : "hover:text-bold font-normal"
+              }
+            >
+              Address
+            </NavLink>
+            <NavLink
+              to={"/user/orders"}
+              className={({ isActive }) =>
+                isActive ? "font-bold" : "hover:text-bold font-normal"
+              }
+            >
+              Orders
+            </NavLink>
+            <Link to={"/"} onClick={logout}>
+              Logout
+            </Link>
           </div>
         </div>
         {/*User Information*/}
         <div className="flex-1">
-          {tabs === "Account" && (
-            <Suspense fallback={<Loading />}>
-              <AccountDetails />
-            </Suspense>
-          )}
-          {tabs === "Address" && (
-            <Suspense fallback={<Loading />}>
-              <Address />
-            </Suspense>
-          )}
+          <Outlet />
         </div>
       </div>
     </Section>
