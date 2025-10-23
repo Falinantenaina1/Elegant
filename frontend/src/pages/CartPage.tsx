91b227daec15e17cdac58feefc73c92af679e59b
falinantenaina1 <nantenainaranaivojaona96@gmail.com>
2025-10-23 08:14:30 +0300

    update cart page menu tabs

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -11,16 +11,16 @@ const CartPage = () => {
     <div className="mx-auto max-w-7xl">
       <div className="px-6 py-6">
         <h2 className="h2">{tabs}</h2>
-        <div className="flex items-center justify-center gap-x-8 overflow-hidden">
+        <div className="flex items-center justify-center gap-x-4 sm:gap-x-8">
           {tabsName.map((tabName, index) => (
             <button
               key={tabName}
-              className={`flex items-center gap-x-2 border-b-2 pb-4 ${tabs === tabName ? "border-black text-black" : index < tabsName.indexOf(tabs) ? "cursor-pointer border-green-400 text-green-400" : "border-transparent text-gray-300"}`}
+              className={`flex items-center border-b-2 pb-1 sm:gap-x-2 sm:pb-4 ${tabs === tabName ? "border-black text-black" : index < tabsName.indexOf(tabs) ? "cursor-pointer border-green-400 text-green-400" : "border-transparent text-gray-300"}`}
               onClick={() => (scroll(0, 0), setTabs(tabName))}
               disabled={index >= tabsName.indexOf(tabs)}
             >
               <div
-                className={`flex size-8 items-center justify-center rounded-full text-white ${tabs === tabName ? "bg-black" : index < tabsName.indexOf(tabs) ? "bg-green-400" : "bg-gray-300"}`}
+                className={`flex size-8 items-center justify-center rounded-full text-white max-sm:hidden ${tabs === tabName ? "bg-black" : index < tabsName.indexOf(tabs) ? "bg-green-400" : "bg-gray-300"}`}
               >
                 {index + 1}
               </div>
